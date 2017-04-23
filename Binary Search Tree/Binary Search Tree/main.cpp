8eefa8e4c2d1700679902e593cf21366732829e9
shaan <shaan.ce@gmail.com>
2017-04-23 17:52:59 +0530

    initial commit for binary search tree project

---
+++ Binary Search Tree/Binary Search Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary Search Tree
+//
+//  Created by Shaan Gontia on 23/04/17.
+//  Copyright © 2017 Shaan Gontia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
